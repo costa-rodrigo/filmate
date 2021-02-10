f8f6d3aad959ef2a47bd6a5a8d2ee95d18eb8d06
Kristen <55934200+kristenIngelman98@users.noreply.github.com>
2021-02-10 13:22:59 -0800

    changed NewGroupFilter screen and deleted PlatformSelection screen

--- screens/Home/NewGroupFilter.js
+++ screens/Home/NewGroupFilter.js
@@ -1,26 +1,76 @@
-import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Button, StyleSheet, TouchableOpacity } from 'react-native';
 import PlatformSelection from './PlatformSelection';
 
+
 export default class NewGroupFilter extends React.Component {
+    state = {
+        // borderColor: 'black',
+        // borderColor2: 'black',
+        // borderWidth: 2,
+        pressed: true
+    };
+
+
+    // false means its been pressed
+    onDisneyPress() {
+        console.log('button was pressed!:', this.state.press);
+        if (this.state.pressed == false) {
+            this.setState({ pressed: true, borderColor1: 'black', borderColor2: 'red'})
+            console.log('disney if', this.state.pressed, 'color changed to: ', this.state.borderColor1)
+            // console.log(this.state.borderColor1)
+            // console.log(this.state.pressed)
+        }
+        else {
+            this.setState({ pressed: false, borderColor1: 'red', borderColor2: 'black'})
+            console.log('disney else', this.state.pressed)
+        }
+        console.log('disney if', this.state.pressed)
+    }
+
+    onNetflixPress() {
+        if (this.state.pressed == false) {
+            this.setState({ pressed: true, borderColor3: 'black', borderColor4: 'red'})
+        } else {
+            this.setState({ pressed: false, borderColor3: 'red', borderColor4: 'black'})
+        }
+    }
+
+    onAmazonPress() {
+        if (this.state.pressed == false) {
+            this.setState({ pressed: true, borderColor5: 'black', borderColor6: 'red'})
+        } else {
+            this.setState({ pressed: false, borderColor5: 'red', borderColor6: 'black'})
+        }
+    }
 
   render() {
     return (
      <View style={styles.screen}>
          <Text style={styles.title}>Filter streaming platform for the group</Text>
+
          <View style={styles.container}>
-             <View style={styles.column1}>
-                 <PlatformSelection />
-                 <PlatformSelection />
-                 <PlatformSelection />
-             </View>
-             <View style={styles.column2}>
-                <PlatformSelection/>
-                <PlatformSelection />
-                <PlatformSelection />
-             </View>
-         </View>
-         
+          <TouchableOpacity
+              style={{borderColor:this.state.borderColor2, borderWidth: 2, padding: 15}}
+              onPress={()=>this.onDisneyPress()}
+                >
+            <Text style={styles.text}>Disney Plus</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+              style={{borderColor:this.state.borderColor4, borderWidth: 2, padding: 15}}
+              onPress={()=>this.onNetflixPress()}
+                >
+            <Text style={styles.text}>Netflix</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+              style={{borderColor:this.state.borderColor6, borderWidth: 2, padding: 15}}
+              onPress={()=>this.onAmazonPress()}
+                >
+            <Text style={styles.text}>Amazon</Text>
+          </TouchableOpacity>
+      </View>
+
          <Button 
             title="Filter"
             onPress={() => {
@@ -41,6 +91,17 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         marginBottom: 60
     },
+    text:{
+        color:'black',
+        // borderColor: 'black',
+        // borderWidth: 1,
+        // borderRadius: 10
+        },
+      container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      },
     // container: {
     //     flexDirection: 'row',
     //     flexWrap: 'wrap',
