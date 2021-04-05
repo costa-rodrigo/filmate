8b8454b2a3effdddbc905023b994ddf909c0816d
Kristen <55934200+kristenIngelman98@users.noreply.github.com>
2021-04-04 19:09:22 -0700

    added new thumb svg

--- svgs/swipe/ThumbsDown.js
+++ svgs/swipe/ThumbsDown.js
@@ -8,12 +8,14 @@ export default function ThumbsDown() {
             <Svg xmlns="http://www.w3.org/2000/svg" width="72" height="72" viewBox="0 0 72 72">
                 <G id="Group_972" data-name="Group 972" transform="translate(-125 -636)">
                     <Path id="Path_148" data-name="Path 148" d="M36,0A36,36,0,1,1,0,36,36,36,0,0,1,36,0Z" transform="translate(125 636)" fill="#242424"/>
-                    <Path id="Icon_material-thumb-up" data-name="Icon material-thumb-up" d="M28.076,1.5H23.244V16h4.832ZM1.5,14.788A2.423,2.423,0,0,0,3.916,17.2h7.623l-1.148,5.521-.036.387a1.818,1.818,0,0,0,.532,1.28l1.28,1.268L20.115,17.7a2.362,2.362,0,0,0,.713-1.7V3.916A2.423,2.423,0,0,0,18.412,1.5H7.54A2.4,2.4,0,0,0,5.317,2.974L1.669,11.49a2.387,2.387,0,0,0-.169.882v2.307l.012.012Z" transform="translate(146.5 658.5)" fill="#d2d5d5"/>
+                    <Path id="Icon_material-thumb-up" data-name="Icon material-thumb-up" d="M28.076,1.5H23.244V16h4.832ZM1.5,14.788A2.423,2.423,0,0,0,3.916,17.2h7.623l-1.148,5.521-.036.387a1.818,1.818,0,0,0,.532,1.28l1.28,1.268L20.115,17.7a2.362,2.362,0,0,0,.713-1.7V3.916A2.423,2.423,0,0,0,18.412,1.5H7.54A2.4,2.4,0,0,0,5.317,2.974L1.669,11.49a2.387,2.387,0,0,0-.169.882v2.307l.012.012Z" transform="translate(146.5 658.5)" fill="#f03349"/>
                 </G>
             </Svg>
         </View>
     )
 }
+
+
 
 
 
